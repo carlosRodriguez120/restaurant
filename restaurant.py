144ad1e4dfafda5c7275e39a6419992473ea2037
carlosrodriguez120 <caralbertrosi@gmail.com>
2022-09-19 11:37:34 -0500

    primer comit del restaurante

--- restaurant.py
+++ restaurant.py
@@ -1,4 +1,6 @@
+from cgitb import text
 from tkinter import *
+from turtle import left
 
 # iniciar aplicacion
 
@@ -12,15 +14,36 @@ aplicacion.resizable(0, 0)
 aplicacion.title("HAMBURGUESAS CACERAS")
 
 # COLOR DE FONDO
-aplicacion.config(bg="burlywood")
+aplicacion.config(bg="LightCyan")
 
 # panel superior
-panelSuperior = Frame(aplicacion, bd=2, relief=FLAT)
+panelSuperior = Frame(aplicacion, bd=5, relief=SUNKEN)
 panelSuperior.pack(side=TOP)
 
 # etiqueta titulo
 etiquetaTitulo = Label(panelSuperior, text="SISTEMA DE FACTURACION",
-                       fg="azure4", font=("dosis", 40), bg="burlywood", width=27)
-etiquetaTitulo.grid(row=0,column=0)
+                       fg="Gray", font=("Dosis", 30), bg="Turquoise", width=42)
+etiquetaTitulo.grid(row=0, column=0)
+# panel izquierdo
+
+panelIzquierdo = Frame(aplicacion, bd=2, relief=SUNKEN)
+panelIzquierdo.pack(side=LEFT)
+
+# panel costos
+panelCostos = Frame(panelIzquierdo, bd=2, relief=SUNKEN)
+panelCostos.pack(side=BOTTOM)
+
+# panel comidas
+
+panelComidas = LabelFrame(panelIzquierdo, text="Comidas", font=(
+    "Dosis", 19, "bold"), bd=2, relief=SUNKEN, fg="Gray")
+panelComidas.pack(side=LEFT)
+
+# panel bebidas
+panelComidas = LabelFrame(panelIzquierdo, text="Comidas ", font=(
+    "Dosis", 19, "bold"), bd=2, relief=SUNKEN, fg="Gray")
+panelComidas.pack(side=LEFT)
+
+
 # evitar que la pantalla se cierre
 aplicacion.mainloop()
